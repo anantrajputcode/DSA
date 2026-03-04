34cdcd4fea6d18ac11d74c57fbd606ebcb27128e
anantrajputcode <anantr2006@gmail.com>
2026-03-04 22:46:48 +0530

    Today's target is completed.

---
+++ 4-mar-26/no_of_ele.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+using namespace std;
+int main()
+{
+    int arr[]{12,12,12,23,34,34,34};
+    cout << "The no of elements is : " << sizeof(arr)/sizeof(int);
+    return 0;
+}
